06fc6b73a5cbe085c65cff58519750df5898621c
AdyPaulino <ariadiny.miranda@gmail.com>
2015-12-10 16:26:34 -0500

    Adding garage sale to model, server route and mainController

---
+++ garageSale/app/models/garage.js
@@ -0,0 +1,18 @@
+//js/app/models/garage.js
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GarageSchema = new Schema({ //internally
+    province: String,
+    country: String,
+    address: String,
+	contactName: String,
+	phoneNumber: String,
+    date: String,
+    time: String,
+    items: [String]
+});
+
+module.exports = mongoose.model('Garage', GarageSchema); //externally 
