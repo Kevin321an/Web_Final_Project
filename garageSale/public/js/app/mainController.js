0f932e0b563d81b32b013be82e1d25efa125121a
AdyPaulino <ariadiny.miranda@gmail.com>
2015-12-10 23:12:36 -0500

    adding form validation

--- garageSale/public/js/app/mainController.js
+++ garageSale/public/js/app/mainController.js
@@ -17,44 +17,55 @@ garageSaleApp.controller('mainController', function($scope, $http, $filter) {
     };
     
     // when submitting the add form, send the text to the node API
-    $scope.createGarageSale = function() {
+    //$scope.createGarageSale = function() {
         
-        //setting formats
-        $scope.garageSale.date = $filter('date')($scope.garageSale.date, "yyyy-MM-dd");
-        $scope.garageSale.time = $filter('date')($scope.garageSale.time, "HH:mm");
+    $scope.submitForm = function(isValid) {
         
-        $scope.garageSale.location = angular.element('#address').val();
-        
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode({'address': $scope.garageSale.location}, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-              $scope.garageSale.latitude = results[0].geometry.location.lat();
-              $scope.garageSale.longitude = results[0].geometry.location.lng();
-                
-                //setting default values for template 
-                $scope.garageSale.image = "images\/photodune-196089-house-xs.jpg";
-                $scope.garageSale.details_url = "map-property.html";
-                $scope.garageSale.ribbon_mark_text = "Garage Sale";
-                $scope.garageSale.ribbon_mark_class = "ribbon-primary";
-                $scope.garageSale.template = "real-estate-map-pop_home";
-                $scope.garageSale.icon = "house-02";
-                $scope.garageSale.options = {bound: true, tags: ["House"]};
-                
-                //after getting all the values for garage, save it
-                $http.post('/api/garages', $scope.garageSale)
-                    .success(function(data) {
-                        $scope.garageSale = {}; // clear the form so our user is ready to enter another
-                        $scope.garageSale = data;
-                    })
-                    .error(function(data) {
-                        console.log('Error: ' + data);
-                    });
-            } else {
-              alert('Geocode was not successful for the following reason: ' + status);
-            }
-        });
+        $scope.submitted = true;
 
-    };
+        // check to make sure the form is completely valid
+        if (isValid) {
+            
+            //setting formats
+            $scope.garageSale.date = $filter('date')($scope.garageSale.date, "yyyy-MM-dd");
+            $scope.garageSale.time = $filter('date')($scope.garageSale.time, "HH:mm");
+
+            $scope.garageSale.location = angular.element('#address').val();
+
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode({'address': $scope.garageSale.location}, function(results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                  $scope.garageSale.latitude = results[0].geometry.location.lat();
+                  $scope.garageSale.longitude = results[0].geometry.location.lng();
+
+                    //setting default values for template 
+                    $scope.garageSale.image = "images\/photodune-196089-house-xs.jpg";
+                    $scope.garageSale.details_url = "map-property.html";
+                    $scope.garageSale.ribbon_mark_text = "Garage Sale";
+                    $scope.garageSale.ribbon_mark_class = "ribbon-primary";
+                    $scope.garageSale.template = "real-estate-map-pop_home";
+                    $scope.garageSale.icon = "house-02";
+                    $scope.garageSale.options = {bound: true, tags: ["House"]};
+
+                    //after getting all the values for garage, save it
+                    $http.post('/api/garages', $scope.garageSale)
+                        .success(function(data) {
+                            $scope.garageSale = {}; // clear the form so our user is ready to enter another
+                            $scope.garageSale = data;
+                        })
+                        .error(function(data) {
+                            console.log('Error: ' + data);
+                        });
+                } else {
+                  alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+          
+        }
+
+      };  
+
+  //  };
     
     $scope.cancelGarageSale = function() {
        $scope.garageSale ={};
